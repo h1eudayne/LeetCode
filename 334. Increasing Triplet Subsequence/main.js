2eae703683939b52d129335a60a5fac8f62c6953
Little Boy's h1eudayne <voduchieu42@gmail.com>
2026-03-03 13:44:03 +0700

    334. Increasing Triplet Subsequence

---
+++ 334. Increasing Triplet Subsequence/main.js
@@ -0,0 +1,15 @@
+var increasingTriplet = function (nums) {
+  let small = Infinity;
+  let big = Infinity;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] <= small) {
+      small = nums[i];
+    } else if (nums[i] <= big) {
+      big = nums[i];
+    } else {
+      return true;
+    }
+  }
+  return false;
+};
